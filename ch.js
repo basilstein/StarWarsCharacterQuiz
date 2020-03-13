20b564a0b0835e79bfbf63a66b6d95ef9340f4dc
basilstein <44881303+basilstein@users.noreply.github.com>
2020-03-13 11:22:36 -0700

    Update ch.js

--- ch.js
+++ ch.js
@@ -2,6 +2,6 @@ function drawCH(){
 	var ch = JSON.parse(localStorage.getItem("ch"));
 	console.log(ch)
 	var st = ch.age + ch.role;
-	document.write("<img src = 'images/" + st + ".PNG'>")
+	document.write("<img src = 'images/" + st + ".png'>")
 }
 drawCH();
